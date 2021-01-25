7b279c0a27aeff63361da2fa8089c111f302da70
NICOLE803 <614461656@qq.com>
2021-01-25 14:51:21 +0800

    First Commit

---
+++ miniprogram/pages/comp_list/comp_list.js
@@ -0,0 +1,92 @@
+// pages/comp_list/comp_list.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    competitionList:[
+      {
+        url: "",
+        image:"../../images/comp_pic.png",
+        name:"xxx",
+        date:"2020-3-5",
+        level:"国家级",
+        number:123,
+      },
+      {
+        url: "",
+        image: "../../images/comp_pic.png",
+        name: "xxx",
+        date: "xxx",
+        level: "xxx",
+        number: 123,
+      },
+      {
+        url: "",
+        image: "../../images/comp_pic.png",
+        name: "xxx",
+        date: "xxx",
+        level: "xxx",
+        number: 123,
+      },
+    ],
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
