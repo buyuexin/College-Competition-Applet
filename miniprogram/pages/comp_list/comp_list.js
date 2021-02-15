6f818fef628698b8a313c148c8c897aebdd5b4fe
NICOLE803 <614461656@qq.com>
2021-02-15 23:20:36 +0800

    修改赛事列表

--- miniprogram/pages/comp_list/comp_list.js
+++ miniprogram/pages/comp_list/comp_list.js
@@ -5,30 +5,39 @@ Page({
    * 页面的初始数据
    */
   data: {
+    type: 0,  // 有两种形式，0或1，赛氪上的列表用0，自己创建的赛事列表用1
+    title:"程序设计",
     competitionList:[
       {
         url: "",
         image:"../../images/comp_pic.png",
-        name:"xxx",
+        name:"2021年第十一届MathorCup高校数学建模挑战赛hhhhhhhh",
         date:"2020-3-5",
         level:"国家级",
-        number:123,
+        registrationTime:"2021.01.01-2021.04.14",  // 报名时间
+        startTime:"2021.04.15-2021.04.19",  // 参赛时间
+        statecolor:"green",  // gray/green/yellow
+        state:"正在报名"  // 已结束/正在报名/正在进行
       },
       {
         url: "",
-        image: "../../images/comp_pic.png",
-        name: "xxx",
-        date: "xxx",
-        level: "xxx",
-        number: 123,
+        image:"../../images/comp_pic.png",
+        name:"MathorCup高校数学建模挑战赛",
+        level:"校级",
+        registrationTime:"2021.01.01-2021.04.14",  // 报名时间
+        startTime:"2021.04.15-2021.04.19",  // 参赛时间
+        statecolor:"gray",  
+        state:"已结束"  
       },
       {
         url: "",
-        image: "../../images/comp_pic.png",
-        name: "xxx",
-        date: "xxx",
-        level: "xxx",
-        number: 123,
+        image:"../../images/comp_pic.png",
+        name:"2021年第十一届MathorCup高校数学建模挑战赛",
+        level:"院级",
+        registrationTime:"2021.01.01-2021.04.14",  // 报名时间
+        startTime:"2021.04.15-2021.04.19",  // 参赛时间
+        statecolor:"yellow",  
+        state:"正在进行"  
       },
     ],
 
