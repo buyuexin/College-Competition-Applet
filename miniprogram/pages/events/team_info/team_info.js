5267b4ba56a96b4785b886c51d7482d4d09c55a9
NICOLE803 <614461656@qq.com>
2021-02-23 12:08:10 +0800

    【添加】队伍详情页面

---
+++ miniprogram/pages/events/team_info/team_info.js
@@ -0,0 +1,71 @@
+// pages/events/team_info/team_info.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    title: "", // 队伍名称
+    name: "", // 发起人姓名
+    college: "", // 所在学校和院系
+    contact: "", // 联系方式
+    content: "", // 队伍简介
+    images:[],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
